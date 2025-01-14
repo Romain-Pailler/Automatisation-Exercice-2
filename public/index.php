efbe7ab3ab8f6d326f00278981a925f9ca2850f9
Romain-Pailler <romain.pailler77@gmail.com>
2025-01-14 09:42:00 +0100

    td2

--- public/index.php
+++ public/index.php
@@ -1,2 +1,17 @@
 <?php
 (require __DIR__ . '/../config/bootstrap.php')->run();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Petit document tout simple</title>
+</head>
+
+<body>
+    <script type="module" src="http://localhost:3000/build/main.js"></script>
+</body>
+
+</html>
